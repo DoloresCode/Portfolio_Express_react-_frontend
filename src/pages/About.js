9ac6da70127acb08283135afefd41c6bccd80159
Dolores Crazover <doloresnetweb@gmail.com>
2023-05-13 13:54:11 -0400

    styling about page done

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,15 +2,20 @@ import { useLoaderData } from "react-router-dom";
 
 function About(props) {
   const about = useLoaderData();
-
+    console.log(about);
   return (
     <div>
       <h2>{about.name}</h2>
       <h3>{about.email}</h3>
       <p>{about.bio}</p>
-      {about.headshotUrl && (
-        <img src={about.headshotUrl} alt="Headshot Dolores Crazover Full 
-        stack software Engineer" />
+      {about.headshot && (
+         <div className="headshot-container">
+        <img
+            src={about.headshot}
+            alt="Headshot Dolores Crazover Full stack software Engineer"
+            className="headshot-image"
+          />
+        </div>
       )}
     </div>
   );
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,21 +1,27 @@
 import { useLoaderData } from "react-router-dom";
 
 function Projects(props) {
+  const projects = useLoaderData();
+  console.log(projects);
 
-  const projects = useLoaderData()
-
-    return projects.map((project) => (
-      <div>
-        <h1>{project.name}</h1>
-        <img src={project.image} alt={project.title || ""} />
-        <a href={project.git}>
-          <button>Github</button>
-        </a>
-        <a href={project.live}>
-          <button>live site</button>
-        </a>
-      </div>
-    ));
+  return (
+    <div>
+      {projects.map((project) => (
+        <div key={project.id} className="project-card">
+          <h1>{project.name}</h1>
+          <div className="image-container">
+            <img src={project.image} alt={project.title || ""} />
+          </div>
+          <a href={project.git} className="button-link">
+            <button>Github</button>
+          </a>
+          <a href={project.live} className="button-link">
+            <button>Live Site</button>
+          </a>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Projects;
