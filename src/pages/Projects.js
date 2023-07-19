68141d3740aff51abf2c6c5eeee21693bf0d0a36
Dolores Crazover <doloresnetweb@gmail.com>
2023-07-18 23:37:02 -0400

    project page updated

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -5,33 +5,21 @@ function Projects(props) {
   console.log(projects);
 
   return (
-    <div>
+    <div className="projects-container">
       {projects.map((project) => (
-        <div key={project.id} className="project-card">
-          <h1>{project.name}</h1>
+        <div key={project.name} className="project-card">
           <div className="image-container">
-            <img src={project.image} alt={project.title || ""} />
+            <img src={project.image} alt={project.name} />
           </div>
-          <p>{project.description}</p>
-          <div className="buttons-container">
-            <a href={project.live} className="button-link">
-              <button className="live-site-button">Live Site</button>
-            </a>
-            {project.git && (
-              <a href={project.git} className="button-link">
-                <button className="github-button">Github</button>
-              </a>
-            )}
-            {project.gitFrontend && (
-              <a href={project.gitFrontend} className="button-link">
-                <button className="github-button">Github Frontend</button>
-              </a>
-            )}
-            {project.gitBackend && (
-              <a href={project.gitBackend} className="button-link">
-                <button className="github-button">Github Backend</button>
-              </a>
-            )}
+          <div className="project-content">
+            <h1 className="project-title">{project.name}</h1>
+            <div className="buttons-container">
+              <a href={project.live} className="live-site-button">Live Site</a>
+              {project.git && <a href={project.git} className="github-button">Github</a>}
+              {project.gitFrontend && <a href={project.gitFrontend} className="github-button">Github Frontend</a>}
+              {project.gitBackend && <a href={project.gitBackend} className="github-button">Github Backend</a>}
+            </div>
+            <p className="project-text">{project.description}</p>
           </div>
         </div>
       ))}
